3f0d9429138843d65aee891330cda9bfbdb3d834
NSKV00 <nicolas.g.g.dsilva@gmail.com>
2025-03-24 21:18:34 -0300

    first commit

---
+++ scrip.js
@@ -0,0 +1,77 @@
+console.log(alert("texte"));
+
+//For atividade 1
+//let palavra = prompt(`Digite uma palavra`)
+//let palavrainvertida = ""
+//if (palavra){
+    //for (let i = palavra.length -1; i >= 0; i--){
+        //palavrainvertida += palavra[i]
+    //}
+    //console.log(`A palavra invertida é: ${palavrainvertida}`)
+//}else{
+    //alert(`Nenhuma palavra foi inserida`)
+//}
+
+//For atividade 2
+//let texto = prompt(`Digite uma palavra/texto`)
+//let resultado = ""
+
+//for (let i =0; i<texto.length;i++){
+    //if(i % 2 ==0){
+        //resultado += texto[i].toUpperCase()
+    //}else{
+        //resultado += texto[i].toLowerCase()
+    //}
+//}
+//console.log(resultado)
+
+//For atividade 3
+//let lista = [1,2,3,4,5,6,7,8,9,15]
+//let resultado = []
+
+//for(let i=0; i<lista.length; i++){
+    //if(lista[i] % 3 ==0 && lista[i] % 5==0){
+        //resultado.push(`boom`)
+    //}else if(lista[i] % 3 == 0){
+        //resultado.push("plic")
+    //}else if(lista[i] % 5 ==0){
+        //resultado.push("plac")
+    //}else {
+        //resultado.push(lista[i])
+    //}
+//}  
+//console.log(resultado)
+
+
+    //Atividade 1 - DESENVOLVA UM ALGORITMO QUE SIMULE UM EMPRÉSTIMO
+      //- O USUÁRIO DEVE INSERIR O VALOR DESEJADO DE EMPRÉSTIMO
+      //- O NÚMERO DE PARCELAS
+      //- OS JUROS EM CADA PARCELA
+
+    //- NO FINAL VOCÊ DEVE EXIBIR:
+      //- VALOR FINAL DO EMPRÉSTIMO COM OS JUROS
+      //- VALOR DE CADA PARCELA COM OS JUROS
+      //- NÚMERO DE PARCELAS
+
+let emprestimo = prompt(`Insira o valor desejado:`)
+let parcelas = prompt(`Insira o numero de parcelas:`)
+let juros = 
+
+    
+      
+      //Atividade 2 - DESENVOLVA UM ALGORIMTO QUE RECEBE UMA PALAVRA DIGITADA PELO USUÁRIO
+        //- VOCÊ DEVE EXIBIR AO FINAL:
+          //- TAMANHO DA PALAVRA
+          //- NÚMERO DE VOGAIS
+          //- PALAVRA INVERTIDA
+
+    
+    
+    
+          //Atividade 3 - DESENVOLVA UM ALGORITMO QUE RECEBA DO USUÁRIO
+        //- UMA LISTA DE NÚMEROS
+        //- UM NÚMERO, PARA SER USADO COMO MULTIPLICADOR
+          //- VOCÊ DEVE EXIBIR AO FINAL
+            //- A SOMA DE TODOS OS NÚMEROS
+            //- UMA LISTA DOS NÚMEROS QUE SÃO DIVISÍVEIS PELO MULPLICADOR INFORMADO.
+    
