007f79bb2d312e51a4d2e1d45c837aece7f27199
NSKV00 <nicolas.g.g.dsilva@gmail.com>
2025-03-26 22:01:04 -0300

    initial

--- scrip.js
+++ scrip.js
@@ -52,20 +52,57 @@ console.log(alert("texte"));
       //- VALOR FINAL DO EMPRÉSTIMO COM OS JUROS
       //- VALOR DE CADA PARCELA COM OS JUROS
       //- NÚMERO DE PARCELAS
+console.log(`Atividade 1`)
 
-let emprestimo = prompt(`Insira o valor desejado:`)
-let parcelas = prompt(`Insira o numero de parcelas:`)
-let juros = 
+let emprestimo = parseFloat(prompt(`Insira o valor desejado:`));{
+    if (isNaN(emprestimo)){
+        alert(`Erro`)
+    }
+}
+let parcelas = parseFloat(prompt(`Insira o numero de parcelas:`));{
+    if (isNaN(parcelas)){ 
+        alert(`Erro`)
+    }
+}
+const juros = 5/100
 
-    
+const montante = emprestimo * Math.pow(1+ juros,parcelas)
+
+console.log(`Valor do empréstimo: ${emprestimo}`)
+console.log(`Quantidade de parcelas: ${parcelas}`)
+console.log(`Taxa de juros: 5% ao mês`)
+const resultado = (`${emprestimo}` *`${1+juros}` ^`${parcelas}`)
+console.log(`Valor final do empréstimo: ${montante.toFixed(2)}`)
+console.log(`Valor da parcela: ${(montante / parcelas).toFixed(2)}`)
       
       //Atividade 2 - DESENVOLVA UM ALGORIMTO QUE RECEBE UMA PALAVRA DIGITADA PELO USUÁRIO
         //- VOCÊ DEVE EXIBIR AO FINAL:
           //- TAMANHO DA PALAVRA
           //- NÚMERO DE VOGAIS
           //- PALAVRA INVERTIDA
+console.log(`Atividade 2`);
 
-    
+let palavra = prompt(`Digite sua palavra:`)
+    console.log(palavra)
+let contvogais = 0
+for (let i=0; i < palavra.length; i++){
+    console.log(palavra[i],i)
+
+if(palavra[i].toLowerCase()==="a" || palavra[i].toLowerCase()==="e" || palavra[i].toLowerCase()==="i" || palavra[i].toLowerCase()==="o" || palavra[i].toLowerCase()==="u"){
+    contvogais++
+}
+}
+console.log(`Sua palavra possuí este numero de vogais: ${contvogais}`)
+
+let palavrainvertida = ""
+if (palavra){
+    for (let i = palavra.length -1; i >= 0; i--){
+        palavrainvertida += palavra[i]
+    }
+    console.log(`A palavra invertida é: ${palavrainvertida}`)
+}else{
+    alert(`Nenhuma palavra foi inserida`)
+}
     
     
           //Atividade 3 - DESENVOLVA UM ALGORITMO QUE RECEBA DO USUÁRIO
@@ -74,4 +111,6 @@ let juros =
           //- VOCÊ DEVE EXIBIR AO FINAL
             //- A SOMA DE TODOS OS NÚMEROS
             //- UMA LISTA DOS NÚMEROS QUE SÃO DIVISÍVEIS PELO MULPLICADOR INFORMADO.
-    
+console.log(`Atividade 3`);
+
+let lista = print(`Digite sua lista:`)
